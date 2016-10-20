e4ca1b311e6729dedf5ca61305fb74d8792091ca
MichelleTaylor <michelle.taylor@red-gate.com>
2016-10-20 14:41:13 +0100

    Use string buffer for looping string append

--- src/main/java/redgatesqlci/Utils.java
+++ src/main/java/redgatesqlci/Utils.java
@@ -56,7 +56,9 @@ public class Utils {
         procParams.add(5, "\"" + sqlCiLocation + "\"");
         procParams.add(6, "-Verbose");
 
-        String longString = "\"" + getPowerShellExeLocation() + "\" -NonInteractive -ExecutionPolicy Bypass -File \"" + sqlCiLocation + "\"" + " -Verbose";
+        StringBuffer longStringBuffer = new StringBuffer();
+
+        longStringBuffer.append("\"" + getPowerShellExeLocation() + "\" -NonInteractive -ExecutionPolicy Bypass -File \"" + sqlCiLocation + "\"" + " -Verbose");
 
         // Here we do some parameter fiddling. Existing quotes must be escaped with three slashes
         // Then, we need to surround the part on the right of the = with quotes iff it has a space.
@@ -76,8 +78,9 @@ public class Utils {
             }
 
             procParams.add(param);
-            longString += " " + fixedParam;
+            longStringBuffer.append(" " + fixedParam);
         }
+        String longString = longStringBuffer.toString();
 
         // Run SQL CI with parameters. Send output and error streams to logger.Map<String, String> vars = new HashMap<String, String>();
         Map<String, String> vars = new HashMap<String, String>();
